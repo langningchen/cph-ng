5849140f760b095ef7da8e76ce05ecc0c410dda4
Langning Chen <chenlangning2009@outlook.com>
2025-12-23 22:16:27 +0800

    test: exclude files from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**'], 
+      exclude: ['src/extension.ts', 'src/application/ports/**']
     },
   },
 });
