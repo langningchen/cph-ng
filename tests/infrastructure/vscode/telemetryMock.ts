43e66e34ad4c5a4c62a933ea19bc779f08a9dd36
Langning Chen <chenlangning2009@outlook.com>
2026-01-31 12:03:58 +0800

    test: enhance telemetry logger

--- tests/infrastructure/vscode/telemetryMock.ts
+++ tests/infrastructure/vscode/telemetryMock.ts
@@ -15,25 +15,27 @@
 // You should have received a copy of the GNU General Public License
 // along with cph-ng.  If not, see <https://www.gnu.org/licenses/>.
 
+import { loggerMock } from '@t/infrastructure/vscode/loggerMock';
 import { mock } from '@t/mock';
 import type { ITelemetry } from '@/application/ports/vscode/ITelemetry';
 
+const logger = loggerMock.withScope('TelemetryMock');
 export const telemetryMock = mock<ITelemetry>();
 telemetryMock.start.mockImplementation((name, props) => {
-  console.log(`[Telemetry Start] ${name}`, props ?? '');
+  logger.debug(`[Telemetry Start] ${name}`, props ?? '');
 
   return (endProps?: Record<string, unknown>) => {
-    console.log(`[Telemetry End] ${name}`, {
+    logger.debug(`[Telemetry End] ${name}`, {
       ...props,
       ...endProps,
     });
   };
 });
 telemetryMock.event.mockImplementation((name, props) => {
-  console.log(`[Telemetry Event] ${name}`, props ?? '');
+  logger.debug(`[Telemetry Event] ${name}`, props ?? '');
 });
 telemetryMock.error.mockImplementation((name, error, props) => {
-  console.error(`[Telemetry Error] ${name}`, {
+  logger.error(`[Telemetry Error] ${name}`, {
     error,
     ...props,
   });
