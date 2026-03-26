e01a15af673c6d99c74b63ecb49e38e64bde50fa
Langning Chen <chenlangning2009@outlook.com>
2026-03-26 21:14:33 +0800

    feat(vscode-webview): add links for Edge and Firefox add-ons in support menu

--- packages/vscode-webview/src/components/actions/support.tsx
+++ packages/vscode-webview/src/components/actions/support.tsx
@@ -15,26 +15,11 @@
 // You should have received a copy of the GNU General Public License
 // along with cph-ng.  If not, see <https://www.gnu.org/licenses/>.
 
-// Copyright (C) 2026 Langning Chen
-//
-// This file is part of cph-ng.
-//
-// cph-ng is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cph-ng is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cph-ng.  If not, see <https://www.gnu.org/licenses/>.
-
 import BugReportIcon from '@mui/icons-material/BugReport';
 import DescriptionIcon from '@mui/icons-material/Description';
+import ExtensionIcon from '@mui/icons-material/Extension';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import GroupIcon from '@mui/icons-material/Group';
 import HelpIcon from '@mui/icons-material/Help';
 import { Divider, ListItemIcon, Menu, MenuItem } from '@mui/material';
 import { CphNgButton } from '@w/components/base/cphNgButton';
@@ -95,7 +80,29 @@ export const HelpButton = () => {
           {t('support.docs')}
         </MenuItem>
         <Divider />
+        <MenuItem
+          onClick={openLink(
+            'https://microsoftedge.microsoft.com/addons/detail/cphng-submit/hfpfdaggmljfccmnfljldojbgfhpfomb',
+          )}
+        >
+          <ListItemIcon>
+            <ExtensionIcon fontSize='small' />
+          </ListItemIcon>
+          {t('support.edgeAddon')}
+        </MenuItem>
+        <MenuItem
+          onClick={openLink('https://addons.mozilla.org/en-US/firefox/addon/cph-ng-submit/')}
+        >
+          <ListItemIcon>
+            <ExtensionIcon fontSize='small' />
+          </ListItemIcon>
+          {t('support.firefoxAddon')}
+        </MenuItem>
+        <Divider />
         <MenuItem onClick={openLink('https://qm.qq.com/q/pXStina3jU')}>
+          <ListItemIcon>
+            <GroupIcon fontSize='small' />
+          </ListItemIcon>
           {t('support.joinQQ')}
         </MenuItem>
       </Menu>
